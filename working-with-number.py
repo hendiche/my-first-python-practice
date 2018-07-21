90baceb215c42477c014b927659b16726e897f87
Unknown <talles_of_destiny@yahoo.com.sg>
2018-07-22 01:07:35 +0900

    Added 5th lesson : working with number

---
+++ working-with-number.py
@@ -0,0 +1,17 @@
+from math import *
+
+print(3 * (4 + 5))
+print(10 % 3)
+
+my_num = 21
+negative_num = -21
+print(str(my_num) + " is my favorite number")
+print(abs(negative_num))
+print(pow(3, 2))
+print(max(4, 6, 21))
+print(min(1, 2, 3))
+print(round(3.2))
+print(round(3.7))
+print(floor(3.7))
+print(ceil(3.7))
+print(sqrt(36))
