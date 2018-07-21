d04782d875e9d883bc8bd723cc10ef137c387528
Unknown <talles_of_destiny@yahoo.com.sg>
2018-07-22 01:07:02 +0900

    Added 3rd lesson : variables and datatypes

---
+++ variable-and-datatype.py
@@ -0,0 +1,9 @@
+char_name = "hendi"
+char_age = "70"
+var_int = 50
+var_bool = False
+
+print("there once was a named " + char_name)
+print("he was " + char_age + " years old")
+print("he really liked the name " + char_name)
+print("but didn't like being " + char_age)
