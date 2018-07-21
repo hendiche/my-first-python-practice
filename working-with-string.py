b33084bdd3cbba2b09aa4af3ced832a42a33ef35
Unknown <talles_of_destiny@yahoo.com.sg>
2018-07-22 01:07:18 +0900

    Added 4th lesson : working with string

---
+++ working-with-string.py
@@ -0,0 +1,8 @@
+phrase = "heNdi"
+print(phrase.lower())
+print(phrase.upper())
+print(phrase.isupper())
+print(len(phrase))
+print(phrase[0])
+print(phrase.index("N"))
+print(phrase.replace("di", "to"))
