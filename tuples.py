d08cf043032d19df76b4043ebca29cf1a358bfe9
Unknown <talles_of_destiny@yahoo.com.sg>
2018-07-23 22:27:42 +0900

    Added 11th lesson : tuples
    *note : tuples is almost like array, but it's unable to change or array with const

---
+++ tuples.py
@@ -0,0 +1,6 @@
+coordinates = (216, 10)  # tuples is like array but its absolute, cannot get change
+# coordinates[1] = 9 #this code will return error, because tuples cannot get change
+print(coordinates[1])
+
+arrCoordinates = [coordinates, (9, 12), (80, 34)]
+print(arrCoordinates)
