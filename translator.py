394d5ec85069574b0914cefe4efdeabcc6d2de84
Unknown <talles_of_destiny@yahoo.com.sg>
2018-07-23 23:30:05 +0900

    Added 23rd lesson : building a translator

---
+++ translator.py
@@ -0,0 +1,22 @@
+# G language
+
+# dog -> dgg
+# cat -> cgt
+# hell -> hgll
+
+
+def translate(phrase):
+    translation = ""
+    for letter in phrase:
+        if letter.lower() in "aiueo":
+            if letter.isupper():
+                translation = translation + "G"
+            else:
+                translation = translation + "g"
+        else:
+            translation = translation + letter
+
+    return translation
+
+
+print(translate(input("enter a phrase: ")))
