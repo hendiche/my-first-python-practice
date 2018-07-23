7d9836604d48dfb23923b2c62707ef7d546fec59
Unknown <talles_of_destiny@yahoo.com.sg>
2018-07-23 23:52:23 +0900

    Added 24th lesson : comments

---
+++ Comments.py
@@ -0,0 +1,9 @@
+# this is a one line comment
+# this is "python legal" comment way using "#"
+
+'''
+this is a multi line comment
+for multiple it's not recommended with this way
+'''
+
+# this statment come's from tutor
